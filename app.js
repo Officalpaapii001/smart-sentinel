4b95c951c611ee929d3dd9d15c905ab2de47562f
Officalpaapii001 <dwngpappy@gmail.com>
2026-05-20 19:06:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+function updateTime(){
 
+  const now = new Date();
+
+  document.getElementById("liveTime").innerHTML =
+    now.toLocaleTimeString();
+
+}
+
+setInterval(updateTime,1000);
+
+updateTime();
